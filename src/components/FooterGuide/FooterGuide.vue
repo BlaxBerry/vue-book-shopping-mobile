966a4918d71732a287ad7d2c6ad0bba2181e22e4
Chen <chenjiaxu333@gmail.com>
2021-04-26 20:50:50 +0900

    底部导航 router-link 声明式导航

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -2,22 +2,25 @@
   <div class="footer_guide">
 
     <!-- 底部导航组件 -->
-   <a class="guide_item active">
+   <router-link to="/home" class="guide_item" :class="$route.path==='/home'?'active':''">
      <span><i class="iconfont icon-home"></i></span>
      <span>首页</span>
-   </a>
-   <a class="guide_item">
+   </router-link>
+
+   <router-link to="/search" class="guide_item" :class="$route.path==='/search'?'active':''">
      <span><i class="iconfont icon-search"></i></span>
-     <span>首页</span>
-   </a>
-   <a class="guide_item">
+     <span>搜索</span>
+   </router-link>
+
+   <router-link to="/order" class="guide_item" :class="$route.path==='/order'?'active':''">
      <span><i class="iconfont icon-order"></i></span>
+     <span>订单</span>
+   </router-link>
+
+   <router-link to="/profile" class="guide_item" :class="$route.path==='/profile'?'active':''">
+     <span><i class="iconfont icon-geren"></i></span>
      <span>首页</span>
-   </a>
-   <a class="guide_item">
-     <span><i class="iconfont icon-gerene"></i></span>
-     <span>首页</span>
-   </a>
+   </router-link>
 
   </div>
 </template>
