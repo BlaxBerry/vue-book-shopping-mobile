0916ede017aa863a3f72eb9e2b98c2644da7c0c7
Chen <chenjiaxu333@gmail.com>
2021-04-26 20:36:59 +0900

    底部导航栏样式

--- src/App.vue
+++ src/App.vue
@@ -26,24 +26,10 @@ export default {
 <style lang="less">
 
 @import "./assets/css/reset.css";
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
 
-// #nav {
-//   padding: 30px;
+@import "./assets/iconfont/iconfont.css";
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
+// App.css
+@import "./assets/css/App/App.css";
 
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-   底部导航组件
+  <div class="footer_guide">
+
+    <!-- 底部导航组件 -->
+   <a class="guide_item active">
+     <span><i class="iconfont icon-home"></i></span>
+     <span>首页</span>
+   </a>
+   <a class="guide_item">
+     <span><i class="iconfont icon-search"></i></span>
+     <span>首页</span>
+   </a>
+   <a class="guide_item">
+     <span><i class="iconfont icon-order"></i></span>
+     <span>首页</span>
+   </a>
+   <a class="guide_item">
+     <span><i class="iconfont icon-gerene"></i></span>
+     <span>首页</span>
+   </a>
+
   </div>
 </template>
 
@@ -11,5 +29,5 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+@import "../../assets/css/FooterGuide/FooterGuide.css";
 </style>
