264dc075a8872c64f1968d58d34d27def75f6790
Chen <chenjiaxu333@gmail.com>
2021-05-12 16:51:35 +0900

    back to top

--- src/components/BackToTop/BackToTop.vue
+++ src/components/BackToTop/BackToTop.vue
@@ -1,28 +1,48 @@
 <template>
-  <div class="backToTop">
-      <!-- <img src="../../assets/images/top.png" alt=""> -->
+  <div id="backToTop" v-show="isShow" @click="clickToGoTop">
+    <!-- <img src="../../assets/images/top.png" alt=""> -->
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.windowScroll);
+  },
+  methods: {
+    windowScroll() {
+      if (
+        window.pageYOffset >= document.querySelector(".tab_content").offsetTop
+      ) {
+        document.getElementById("backToTop").style.display = "block";
+      } else {
+        document.getElementById("backToTop").style.display = "none";
+      }
+    },
+    clickToGoTop() {
+      window.scroll(0,0)
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.backToTop {
-    position: fixed;
-    bottom: 80px;
-    right: 20px;
-    width: 50px;
-    height: 50px;
-    padding: 10px;
-    background:rgba(204, 204, 204, 0.5) url("../../assets/images/top.png") no-repeat center;
-    background-size: 40px auto;
-    border-radius: 50%;
-    box-shadow: 2px 2px 5px #333;
-
-
+#backToTop {
+  position: fixed;
+  bottom: 80px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  padding: 10px;
+  background: rgba(250, 250, 250, 0.6) url("../../assets/images/top.png")
+    no-repeat center;
+  background-size: 40px auto;
+  border-radius: 50%;
+  box-shadow: 2px 2px 5px #333;
 }
 </style>
