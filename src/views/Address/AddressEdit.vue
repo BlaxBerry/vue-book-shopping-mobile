229446a91fb8ed174ee24d364d88081157ef9055
Chen <chenjiaxu333@gmail.com>
2021-05-13 05:58:40 +0900

    AddressList +AddressEdit 页面完善

--- src/views/Address/AddressEdit.vue
+++ src/views/Address/AddressEdit.vue
@@ -4,165 +4,171 @@
     <!-- 标题栏 -->
     <HeaderBar></HeaderBar>
 
-
     <van-address-edit
-        :area-list="areaList"
-        :show-postal="true"
-        :show-delete="typeIsEdit"
-        :show-set-default="true"
-        :area-columns-placeholder="['请选择', '请选择', '请选择']"
-        :address-info="addressInfo"
-        @save="onSave"
-        @delete="onDelete"
+      :area-list="areaList"
+      :show-postal="true"
+      :show-delete="typeIsEdit"
+      :show-set-default="true"
+      :area-columns-placeholder="['请选择', '请选择', '请选择']"
+      :address-info="addressInfo"
+      @save="onSave"
+      @delete="onDelete"
     />
-
-
   </div>
 </template>
 
 <script>
 // 导入 头部标题栏
-import HeaderBar from "@/components/HeaderBar/HeaderBar.vue"
+import HeaderBar from "@/components/HeaderBar/HeaderBar.vue";
 //Vant 官方提供了一份默认的省市区数据
-import { areaList } from '@vant/area-data';
+import { areaList } from "@vant/area-data";
 
 // 导入 api接口
 import {
   // 获取当前地址的信息
-  GetAddressDetail,  
+  GetAddressDetail,
   // 添加地址
   AddUserAddress,
   // 编辑地址
   EditAddress,
   // 删除地址
-  DeleteAddress
-
-} from "@/network/api.js"
+  DeleteAddress,
+} from "@/network/api.js";
 
 export default {
-    data() {
-        return {
-          // VantUI自带 中国县市区地名
-          areaList:areaList,
+  data() {
+    return {
+      // VantUI自带 中国县市区地名
+      areaList: areaList,
 
-          // 删除按钮显示/隐藏
-          typeIsEdit:true,
+      // 删除按钮显示/隐藏
+      typeIsEdit: true,
 
-          // VantUI 自带的默认参数，表单内容的初始值
-          // 修改时自动充填表格的初始默认内容
-          addressInfo:{}
-        }
-    },
+      // VantUI 自带的默认参数，表单内容的初始值
+      // 修改时自动充填表格的初始默认内容
+      addressInfo: {},
+    };
+  },
 
-    methods: {
-        // VantUI 自带 保存表单数据
-        onSave(content) {
-            // VantUI默认参数content 对象形式获得输入的内容
-            // console.log("添加的地址",content);
-            let params = {
-                  name: content.name,
-                  phone:content.tel,
-                  province:content.province,
-                  city:content.city,
-                  county:content.county,
-                  address:content.addressDetail,
-                  is_default:content.isDefault ? 1 : 0
-                  }
+  methods: {
+    // VantUI 自带 保存表单数据
+    onSave(content) {
+      // VantUI默认参数content 对象形式获得输入的内容
+      // console.log("添加的地址",content);
+      let params = {
+        name: content.name,
+        phone: content.tel,
+        province: content.province,
+        city: content.city,
+        county: content.county,
+        address: content.addressDetail,
+        is_default: content.isDefault ? 1 : 0,
+      };
 
-            if(this.$route.query.type == "add"){
-                // 发送添加地址请求
-                AddUserAddress(params).then(res=>{
-                    console.log(res);
-                    // 提示消息
-                    this.$toast.loading({
-                    message: '添加成功...',
-                    forbidClick: true,
-                    });
-                    // 跳转页面
-                    setTimeout(()=>{
-                      this.$router.go(-1)
-                    },1000)
-                })
-            }else if(this.$route.query.type == "edit"){
-                // 发送修改请求
-                EditAddress(this.$route.query.addressId, params).then(res=>{
-                  // console.log(res);
-                    // 提示消息
-                    this.$toast.loading({
-                    message: '修改成功...',
-                    forbidClick: true,
-                    });
-                    // 跳转页面
-                    setTimeout(()=>{
-                      this.$router.go(-1)
-                    },1000)
-                })
-            }
-        },
-
-        // 删除表单 
-        onDelete() {
-            // 发送删除地址请求
-            DeleteAddress(this.$route.query.addressId)
-          
-            // 提示消息
-            this.$toast.loading({
-            message: '删除成功...',
+      if (this.$route.query.type == "add") {
+        // 发送添加地址请求
+        AddUserAddress(params).then((res) => {
+          console.log(res);
+          // 提示消息
+          this.$toast.loading({
+            message: "添加成功...",
             forbidClick: true,
-            });
-
-            // 跳转页面
-            setTimeout(()=>{
-              this.$router.go(-1)
-            },1000)
-
-        },
-
-    },
-
-    components:{
-        HeaderBar
-    },
-
-    created(){
-      // console.log(areaList);
-      // VantUI 提供的区地址域数据
-      this.city_list = areaList.city_list
-      this.county_list = areaList.county_list
-      this.province_list = areaList.province_list
-      // console.log(this.province_list);
-      
-      // 判断路由地址中参数type 是 add 还是 edit，来决定是否添加删除按钮
-      this.typeIsEdit = (this.$route.query.type == "edit")?true:false;
-
-      // 判断是否在 type是否是 edit
-      if(this.$route.query.type == 'edit'){
-          GetAddressDetail(this.$route.query.addressId).then(res=>{
-              console.log(res);
-              console.log(this.addressInfo);
-
-              // VantUI 自带的默认参数，表单内容的初始值
-              this.addressInfo = {
-                  id: res.id,
-                  name: res.name,
-                  tel: res.phone,
-                  province:res.province,
-                  city:res.city,
-                  country:res.country,
-                  addressDetail:res.address,
-                  isDefault: res.is_default?true:false,
-              }
-              // console.log(this.addressInfo);
-          }
-      )
+          });
+          // 跳转页面
+          setTimeout(() => {
+            this.$router.go(-1);
+          }, 1000);
+        });
+      } else if (this.$route.query.type == "edit") {
+        // 发送修改请求
+        EditAddress(this.$route.query.addressId, params).then((res) => {
+          // console.log(res);
+          // 提示消息
+          this.$toast.loading({
+            message: "修改成功...",
+            forbidClick: true,
+          });
+          // 跳转页面
+          setTimeout(() => {
+            this.$router.go(-1);
+          }, 1000);
+        });
       }
-      
+    },
+
+    // 删除表单
+    onDelete() {
+      // 发送删除地址请求
+      DeleteAddress(this.$route.query.addressId);
+
+      // 提示消息
+      this.$toast.loading({
+        message: "删除成功...",
+        forbidClick: true,
+      });
+
+      // 跳转页面
+      setTimeout(() => {
+        this.$router.go(-1);
+      }, 1000);
+    },
+  },
+
+  components: {
+    HeaderBar,
+  },
+
+  created() {
+    // console.log(areaList);
+    // VantUI 提供的区地址域数据
+    this.city_list = areaList.city_list;
+    this.county_list = areaList.county_list;
+    this.province_list = areaList.province_list;
+    // console.log(this.province_list);
+
+    // 判断路由地址中参数type 是 add 还是 edit，来决定是否添加删除按钮
+    this.typeIsEdit = this.$route.query.type == "edit" ? true : false;
+
+    // 判断是否在 type是否是 edit
+    if (this.$route.query.type == "edit") {
+      GetAddressDetail(this.$route.query.addressId).then((res) => {
+        console.log(res);
+        console.log(this.addressInfo);
+
+        // VantUI 自带的默认参数，表单内容的初始值
+        this.addressInfo = {
+          id: res.id,
+          name: res.name,
+          tel: res.phone,
+          province: res.province,
+          city: res.city,
+          country: res.country,
+          addressDetail: res.address,
+          isDefault: res.is_default ? true : false,
+        };
+        // console.log(this.addressInfo);
+      });
     }
-}
+  },
+};
 </script>
 
 <style lang="less" scoped>
 #addressEdit {
   padding-top: 50px;
+
+  /deep/button {
+    border-radius: 0;
+  }
+  /deep/.van-button--danger {
+    width: 100%;
+    height: 50px;
+    background-color: #42b983;
+    border: 0;
+    border-radius: 0;
+    width: 100%;
+    font-size: 16px;
+    color: #fff;
+  }
 }
 </style>
--- src/views/Address/AddressList.vue
+++ src/views/Address/AddressList.vue
@@ -1,156 +1,162 @@
 <template>
   <div id="addressList">
     <!-- 标题栏 -->
-    <HeaderBar>
-    </HeaderBar>
-
+    <HeaderBar> </HeaderBar>
 
     <!-- 空白状态 -->
-    <van-empty  v-if="!list.length"
-        description="还没有地址，快去添加" >
-        <van-button block class="bottom-button"
-            @click="onAdd">添加地址</van-button>
+    <van-empty v-if="!list.length" description="还没有地址，快去添加">
+      <van-button block class="bottom-button" @click="onAdd"
+        >添加地址</van-button
+      >
     </van-empty>
-        
 
     <!-- 列表 -->
     <van-address-list
-          v-if="list.length"
-          v-model="chosenAddressId"
-          :list="list"
-          default-tag-text="默认"
-          @add="onAdd"
-          @edit="onEdit"
-          @select="onSelect"
+      v-if="list.length"
+      v-model="chosenAddressId"
+      :list="list"
+      default-tag-text="默认"
+      @add="onAdd"
+      @edit="onEdit"
+      @select="onSelect"
     />
-
-
   </div>
 </template>
 
 <script>
 // 导入 头部标题栏
-import HeaderBar from "@/components/HeaderBar/HeaderBar.vue"
+import HeaderBar from "@/components/HeaderBar/HeaderBar.vue";
 
 // 导入 api接口
 import {
   // 获取地址列表
   GetAddressList,
   // 设为默认地址
-  SetDefaultAddress
-
-} from "@/network/api.js"
+  SetDefaultAddress,
+} from "@/network/api.js";
 
 export default {
   data() {
     return {
       // 所有的地址列表
-      list:[],
+      list: [],
       // checkbox选中，address地址的id
-      chosenAddressId: '0',
+      chosenAddressId: "0",
       // 跳转的页面是add新增还是edit编辑
-      type:'add',
-    
+      type: "add",
     };
   },
 
-  components:{
-      HeaderBar
+  components: {
+    HeaderBar,
   },
 
   methods: {
     // 添加地址按钮
     onAdd() {
-        // 跳转到 编辑地址页面，修改路由属性 type为 add新增
-        this.$router.push({
-          path:'/address_edit',
-          query:{
-            type:'add'
-          }
-        })  
+      // 跳转到 编辑地址页面，修改路由属性 type为 add新增
+      this.$router.push({
+        path: "/address_edit",
+        query: {
+          type: "add",
+        },
+      });
     },
 
     // 编辑地址
     // VantUI 默认参数item是点击对象
     onEdit(item) {
-        // 跳转到 编辑地址页面,修改路由属性 type为 edit编辑
-        this.$router.push({
-          path:'/address_edit',
-          query:{
-            type:'edit',
-            addressId:item.id
-          }
-        })  
+      // 跳转到 编辑地址页面,修改路由属性 type为 edit编辑
+      this.$router.push({
+        path: "/address_edit",
+        query: {
+          type: "edit",
+          addressId: item.id,
+        },
+      });
     },
 
     // 复选框状态变化时
-    onSelect(item){
+    onSelect(item) {
       // 获取选中的地址的 id
       console.log(item.id);
 
       // 发送请求，设为默认
-      SetDefaultAddress(item.id).then(res=>{
-          console.log(res);
-          // 刷新页面
-          this.$router.go(0)
-          
-      })
-
-    }
+      SetDefaultAddress(item.id).then((res) => {
+        console.log(res);
+        // 刷新页面
+        this.$router.go(0);
+      });
+    },
   },
 
-  created(){
-      // 提示消息
-        this.$toast.loading({
-        message: '加载中...',
-        forbidClick: true,
+  created() {
+    // 提示消息
+    this.$toast.loading({
+      message: "加载中...",
+      forbidClick: true,
+    });
+
+    // 请求 地址列表
+    GetAddressList().then((res) => {
+      console.log("获取的全部地址列表", res);
+
+      // 获取所有 地址列表，并渲染
+      if ((res.length = 0)) {
+        this.list = [];
+        return;
+      } else {
+        res.data.map((item) => {
+          this.list.push({
+            id: item.id,
+            name: item,
+            name,
+            tel: item.phone,
+            address: item.province + item.city + item.county + item.address,
+            //数字转为bollean
+            isDefault: !!item.is_default,
+          });
+        });
+        // console.log(this.list);
+      }
+
+      // checkebbox 选中状态
+      // 根据 获取的地址list中 is_default属性是true的，获取其id并赋值给 控制选中的变量
+      this.list.filter((item) => {
+        if (item.isDefault) {
+          // console.log(item.id);
+          this.chosenAddressId = item.id;
+        }
       });
-
-      // 请求 地址列表
-      GetAddressList().then(res=>{
-          console.log("获取的全部地址列表",res);
-
-          // 获取所有 地址列表，并渲染
-          if(res.length = 0){
-              this.list = []
-              return
-          }else{
-              res.data.map(item=>{
-                  this.list.push({
-                      id: item.id,
-                      name:item,name,
-                      tel:item.phone,
-                      address:item.province + item.city + item.county + item.address,
-                      //数字转为bollean
-                      isDefault:!!item.is_default})
-              })
-              // console.log(this.list);
-          }
-
-          // checkebbox 选中状态
-          // 根据 获取的地址list中 is_default属性是true的，获取其id并赋值给 控制选中的变量
-          this.list.filter(item=>{
-              if(item.isDefault){
-                  // console.log(item.id);
-                  this.chosenAddressId = item.id
-              }
-          })
-
-      })
-  }
+    });
+  },
 };
 </script>
 
 <style lang="less" scoped>
 #addressList {
-    padding: 50px 0;
+  padding: 50px 0;
 
-    .van-address-list {
-        padding: 20px 0;
-        
-        .van-address-list__bottom {
-            bottom: 50px
-        }
+  .van-address-list {
+    padding: 20px 0;
+
+    .van-address-list__bottom {
+      bottom: 50px;
     }
+  }
+  .van-address-list__bottom {
+    padding: 0;
+  }
+  button {
+    width: 100%;
+    height: 50px;
+    background-color: #42b983;
+    border: 0;
+    border-radius: 0;
+    margin: 0;
+    width: 100%;
+    font-size: 16px;
+    color: #fff;
+  }
 }
 </style>
